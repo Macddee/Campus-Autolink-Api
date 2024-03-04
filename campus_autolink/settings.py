45c7747f6b9dee94a957d787692eaac81e15cc35
Macdonald Manana <macddeemanana@gmail.com>
2024-03-04 18:06:51 +0200

    Heroku updates

--- campus_autolink/settings.py
+++ campus_autolink/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import django_heroku
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -136,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         "knox.auth.TokenAuthentication",
 #     ]
 # }
+
+django_heroku.settings(locals())
